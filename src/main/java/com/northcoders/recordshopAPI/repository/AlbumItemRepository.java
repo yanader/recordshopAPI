f3c229997fc594b932f9b075e19793c8eaa51932
ste <mrbarrow@hotmail.co.uk>
2024-06-07 10:38:41 +0100

    refactor: changed repository names

--- src/main/java/com/northcoders/recordshopAPI/repository/AlbumItemRepository.java
+++ src/main/java/com/northcoders/recordshopAPI/repository/AlbumItemRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RecordItemRepository extends CrudRepository<Album, Integer> {
+public interface AlbumItemRepository extends CrudRepository<Album, Integer> {
 }
--- src/main/java/com/northcoders/recordshopAPI/repository/StockItemRepository.java
+++ src/main/java/com/northcoders/recordshopAPI/repository/StockItemRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Stock extends CrudRepository<Stock, Integer> {
+public interface StockItemRepository extends CrudRepository<StockItemRepository, Integer> {
 }
