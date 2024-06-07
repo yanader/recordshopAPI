31fd26e4adf642e990b41796651be6435c736fb1
ste <mrbarrow@hotmail.co.uk>
2024-06-07 10:10:36 +0100

    initial commit

---
+++ src/main/java/com/northcoders/recordshopAPI/RecordshopApiApplication.java
@@ -0,0 +1,13 @@
+package com.northcoders.recordshopAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordshopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordshopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northcoders/recordshopAPI/RecordshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.recordshopAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordshopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
