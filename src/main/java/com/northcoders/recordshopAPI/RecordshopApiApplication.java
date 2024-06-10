af50a9e76f3e5474e45811ed667ad3ec5540a56e
ste <mrbarrow@hotmail.co.uk>
2024-06-10 16:42:15 +0100

    updateTODO

--- src/main/java/com/northcoders/recordshopAPI/RecordshopApiApplication.java
+++ src/main/java/com/northcoders/recordshopAPI/RecordshopApiApplication.java
@@ -1,5 +1,6 @@
 package com.northcoders.recordshopAPI;
 
+import com.northcoders.recordshopAPI.view.UserInterface;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -12,6 +13,7 @@ public class RecordshopApiApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(RecordshopApiApplication.class, args);
+		// new UserInterface().run();
 	}
 
 }
